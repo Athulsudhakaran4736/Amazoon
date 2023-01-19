2899d25f0ae5f48941e59975fc0a0992660a23c7
Athul <athulsudhakaran4736@gmail.com>
2023-01-19 12:31:21 +0530

    commit-8

--- frontend/src/Screen/ProductScreen.js
+++ frontend/src/Screen/ProductScreen.js
@@ -58,7 +58,7 @@ function ProductScreen() {
 
     ctxdispatch({
       type: 'CART_ADD_ITEMS',
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
   };
   return loading ? (
