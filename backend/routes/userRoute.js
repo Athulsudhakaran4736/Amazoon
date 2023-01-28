82a7b3f147b244475f871349dd4d7068c280ea11
Athul <athulsudhakaran4736@gmail.com>
2023-01-28 13:14:27 +0530

    commit-13

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
+import expressAsyncHandler from 'express-async-handler';
+
+const userRoute = express.Router();
+
+userRoute.post(
+  '/signIn',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    } else res.status(401).send({ message: 'Invalid password or email' });
+  })
+);
+
+export default userRoute;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRoute from './routes/seedRoute.js';
 import productRoute from './routes/productRoute.js';
+import userRoute from './routes/userRoute.js';
 
 dotenv.config();
 mongoose
@@ -17,8 +18,16 @@ mongoose
 
 const app = express();
 
+app.use(express.json()); //form data in the post request will be in json format
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRoute);
 app.use('/api/products', productRoute);
+app.use('/api/users', userRoute);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
